cc29cac62bbc1defd03b77489acde8353c0b6ca0
Ashik Ahmed <md.ashikahmed@outlook.com>
2025-01-26 17:33:00 +0600

    footer fixed

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,15 +5,15 @@ import Header from "@/components/Header.vue";
 
 <template>
 
-  <div class="max-w-sm mx-auto relative">
+  <div class="relative">
     <Header/>
 
     <main class="scrollbar h-screen pb-30 ">
       <slot/>
     </main>
 
-    <!-- Bottom Navigation -->
-    <nav class="fixed max-w-sm mx-auto bottom-0 left-0 right-0 bg-white flex justify-around items-center py-2">
+    <!-- Bottom Navigation max-w-sm mx-auto-->
+    <nav class="fixed  bottom-0 left-0 right-0 bg-white flex justify-around items-center py-2">
       <a href="/" class="flex flex-col items-center text-sm text-purple-700">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
