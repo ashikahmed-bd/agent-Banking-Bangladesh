52989f86a4c9c08ffb78825d3ac7cf2a53382dea
Ashik Ahmed <md.ashikahmed@outlook.com>
2025-02-11 13:22:21 +0600

    customer delete

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -2,8 +2,11 @@
 import Default from "@/layouts/Default.vue";
 import {useAuthStore} from "@/stores/auth.js";
 import Swal from "sweetalert2";
+import {storeToRefs} from "pinia";
 
 const authStore = useAuthStore();
+
+const {user} = storeToRefs(authStore);
 
 const logout = async () => {
   const result = await Swal.fire({
@@ -26,7 +29,7 @@ const logout = async () => {
 
 <template>
   <Default>
-
+    
     <section class="py-2">
       <div class="bg-white h-screen rounded-md p-4">
         <h3 class="font-semibold text-lg border-b border-dashed border-gray-300 ">Profile info</h3>
@@ -45,7 +48,7 @@ const logout = async () => {
                 </svg>
               </li>
 
-              <li class="flex items-center justify-between py-2">
+              <li v-if="user.type === 'owner'" class="flex items-center justify-between py-2">
                 <a href="#" class="flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
@@ -58,7 +61,7 @@ const logout = async () => {
               </li>
 
 
-              <li class="flex items-center justify-between py-2">
+              <li v-if="user.type === 'owner'"  class="flex items-center justify-between py-2">
                 <RouterLink :to="{name: 'users.create'}" class="flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
